c3df1d378063402ea31b15779e5d39fdfd58f97c
liu_chonghui <liuchonghui@139.com>
2016-04-26 15:44:20 +0800

    commit changes

--- SinaWidgetDemo/demo/src/com/sina/sinawidgetdemo/fragment/TimerGalleryFragment.java
+++ SinaWidgetDemo/demo/src/com/sina/sinawidgetdemo/fragment/TimerGalleryFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.sina.sinawidgetdemo.R;
 import com.sina.sinawidgetdemo.custom.timergallery.JumpableImage;
@@ -42,12 +43,16 @@ public class TimerGalleryFragment extends BaseFragment {
 	private void initData(){
 		focusImages.clear();
 		JumpableImage image1 = new JumpableImage();
+		image1.content="image1";
 		image1.img="http://github.com/liuchonghui/SinaWidgetDemo/blob/master/SinaWidgetDemo/demo/res/drawable-hdpi/banner1.jpg";
 		JumpableImage image2 = new JumpableImage();
+		image2.content="image2";
 		image2.img="https://github.com/liuchonghui/SinaWidgetDemo/blob/master/SinaWidgetDemo/demo/res/drawable-hdpi/banner2.jpg";
 		JumpableImage image3 = new JumpableImage();
+		image3.content="image3";
 		image3.img="https://github.com/liuchonghui/SinaWidgetDemo/blob/master/SinaWidgetDemo/demo/res/drawable-hdpi/banner3.jpg";
 		JumpableImage image4 = new JumpableImage();
+		image4.content="image4";
 		image4.img="https://github.com/liuchonghui/SinaWidgetDemo/blob/master/SinaWidgetDemo/demo/res/drawable-hdpi/banner4.jpg";
 		focusImages.add(image1);
 		focusImages.add(image2);
@@ -61,7 +66,6 @@ public class TimerGalleryFragment extends BaseFragment {
 		LinearLayout bannerLayout = (LinearLayout) view.findViewById(R.id.main_layout);
 		if (focusImages != null && focusImages.size() != 0) {
 			gallery = new TimerGallery(getActivity());
-			gallery.setCycle(true);
 			gallery.init(getActivity(), focusImages);
 			SpecialSellingGalleryAdapter bigAdapter;
 			bigAdapter = new SpecialSellingGalleryAdapter(
@@ -70,6 +74,13 @@ public class TimerGalleryFragment extends BaseFragment {
 			gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 				@Override
 				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					if (focusImages != null && focusImages.size() > position % focusImages.size()) {
+						JumpableImage image = focusImages.get(position % focusImages.size());
+						if (image != null && image.content != null) {
+							Toast.makeText(getActivity(), image.content,
+									Toast.LENGTH_SHORT).show();
+						}
+					}
 				}
 			});
 			bannerLayout.addView(gallery);
